2eac2a87645fa2fc3ba2dcf78406d6716d6a297d
Noelia Barrionuevo <noe.br97@gmail.com>
2026-05-12 10:46:01 +0200

    configuracion de app module y main

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { AuthModule } from './auth/auth.module';
+// import { MascotasModule } from './mascotas/mascotas.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    //módulo de configuración
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
+    
+    UsuariosModule,
+
+    // AuthModule,
+    UsuariosModule,
+    // MascotasModule,
+  ],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import kleur from 'kleur';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ["http://localhost:9080", "http://127.0.0.1:9080"],
+    methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+  });
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
+
+  console.log(
+    kleur.green('🌐 ') +
+    kleur.green().bold('Application is running on: ') +
+    kleur.cyan(await app.getUrl())
+  );
 }
 bootstrap();
