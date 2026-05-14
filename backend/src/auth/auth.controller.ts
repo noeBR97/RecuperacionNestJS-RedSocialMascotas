da5ca8f8e00ebbe879eb6763c280b2581938cf4a
Noelia Barrionuevo <noe.br97@gmail.com>
2026-05-14 19:00:55 +0200

    meto el registro en auth para que sea publico

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,13 +1,20 @@
 import { Controller, Post, Body, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { CreateUsuarioDto } from 'src/usuarios/dto/create-usuario.dto';
+import { UsuariosService } from 'src/usuarios/usuarios.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService, private usuariosService: UsuariosService) {}
 
     @Post('login')
     async login(@Body() loginDto: LoginDto) {
         return this.authService.login(loginDto)
+    }
+
+    @Post('registro')
+    create(@Body() createUsuarioDto: CreateUsuarioDto) {
+    return this.usuariosService.create(createUsuarioDto);
     }
 }
